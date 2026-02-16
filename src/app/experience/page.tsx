c46e1670ac725b30c258c49f4644fb1daead40bf
Bryan Tan <bthk2151@gmail.com>
2026-02-16 22:17:00 +0800

    Add new Trading Bot project

--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -1,6 +1,7 @@
 import imgSeb from '@/assets/companies/seb.png';
 import imgFusionex from '@/assets/companies/fusionex.png';
 import imgCorematter from '@/assets/companies/corematter.png';
+import imgTradingBot from '@/assets/projects/trading-bot.png';
 import imgPortfolioWebsite from '@/assets/projects/portfolio-website.png';
 import imgPfp from '@/assets/projects/pfp.png';
 import imgC19Memorial from '@/assets/projects/c19memorial.png';
@@ -35,6 +36,56 @@ const careerTimelineData = [
     }
 ];
 
+const projectsData = [
+    {
+        title: "Sentiment Trading Bot",
+        description: "Alongside a partner, we built a sentiment-driven trading bot using the Alpaca Broker Trading API for live execution and the Lumibot library to design and backtest our strategy.",
+        imgSrc: imgTradingBot,
+        skills: [
+            { text: "Python", iconClass: "devicon-python-plain" },
+            { text: "Jupyter Notebook", iconClass: "devicon-jupyter-plain" }
+        ],
+        projectUrl: "https://bthk2151.dev/sentiment-trading-bot.html",
+        githubUrl: "https://github.com/bthk2151/SentimentTradingBot"
+    },
+    {
+        title: "Portfolio Website",
+        description: "I challenged myself to design and build this pure frontend portfolio website in under a week — and I am pleased with the outcome. In 2026, it was rewritten using Next.js!",
+        imgSrc: imgPortfolioWebsite,
+        skills: [
+            { text: "React.js", iconClass: "devicon-react-plain" },
+            { text: "Next.js", iconClass: "devicon-nextjs-plain" },
+            { text: "Bootstrap", iconClass: "devicon-bootstrap-plain" },
+            { text: "Vercel", iconClass: "devicon-vercel-plain" }
+        ],
+        projectUrl: "https://bthk2151.dev",
+        githubUrl: "https://github.com/bthk2151/portfolio-website"
+    },
+    {
+        title: "c19memorialmy",
+        description: "The Covid-19 Memorial Malaysia website is a not-for-profit volunteer-run online memorial, for the sole purpose of remembering those who have lost their battles to Covid-19 in the country. <a href='https://codeblue.galencentre.org/2021/09/online-memorial-remembers-malaysians-killed-by-covid-19/' target='_blank'>Here</a> is one of the public mentions.",
+        imgSrc: imgC19Memorial,
+        skills: [
+            { text: "React.js", iconClass: "devicon-react-plain" },
+            { text: "Tailwind CSS", iconClass: "devicon-tailwindcss-plain" }
+        ],
+        projectUrl: "https://c19memorial.my/",
+        githubUrl: "https://github.com/c19memorialmy/frontend"
+    },
+    {
+        title: "Personal Finance Project (PFP)",
+        description: "A web application serving as a personal finance tracking solution for >1 user. A little experiment to explore beyond my familiar .NET development stack. ",
+        imgSrc: imgPfp,
+        skills: [
+            { text: "React.js", iconClass: "devicon-react-plain" },
+            { text: "Material UI", iconClass: "devicon-materialui-plain" },
+            { text: "Django", iconClass: "devicon-python-plain" },
+            { text: "MySQL", iconClass: "devicon-mysql-plain" }
+        ],
+        githubUrl: "https://github.com/bthk2151/PersonalFinanceProject"
+    }
+];
+
 export default function Experience() {
     return (
         <div className="container page-content">
@@ -62,48 +113,18 @@ export default function Experience() {
                     <h1 className="display-6 mb-3">Project Portfolio</h1>
                     <p className="lead mb-4">Welcome to my portfolio — a curated showcase of private and public projects I have worked on over the years, with more to come.</p>
                     <div className="row">
-                        <div className="col-md-4 mb-4">
-                            <ProjectCard
-                                title="Portfolio Website"
-                                description="I challenged myself to design and build this pure frontend portfolio website in under a week — and I am pleased with the outcome. In 2026, it was rewritten using Next.js!"
-                                imgSrc={imgPortfolioWebsite}
-                                skills={[
-                                    { text: "React.js", iconClass: "devicon-react-plain" },
-                                    { text: "Next.js", iconClass: "devicon-nextjs-plain" },
-                                    { text: "Bootstrap", iconClass: "devicon-bootstrap-plain" },
-                                    { text: "Vercel", iconClass: "devicon-vercel-plain" }
-                                ]}
-                                projectUrl="https://bthk2151.dev"
-                                githubUrl="https://github.com/bthk2151/portfolio-website"
-                            />
-                        </div>
-                        <div className="col-md-4 mb-4">
-                            <ProjectCard
-                                title="c19memorialmy"
-                                description="The Covid-19 Memorial Malaysia website is a not-for-profit volunteer-run online memorial, for the sole purpose of remembering those who have lost their battles to Covid-19 in the country. <a href='https://codeblue.galencentre.org/2021/09/online-memorial-remembers-malaysians-killed-by-covid-19/' target='_blank'>Here</a> is one of the public mentions."
-                                imgSrc={imgC19Memorial}
-                                skills={[
-                                    { text: "React.js", iconClass: "devicon-react-plain" },
-                                    { text: "Tailwind CSS", iconClass: "devicon-tailwindcss-plain" }
-                                ]}
-                                projectUrl="https://c19memorial.my/"
-                                githubUrl="https://github.com/c19memorialmy/frontend"
-                            />
-                        </div>
-                        <div className="col-md-4 mb-4">
-                            <ProjectCard
-                                title="Personal Finance Project (PFP)"
-                                description="A web application serving as a personal finance tracking solution for >1 user. A little experiment to explore beyond my familiar .NET development stack. "
-                                imgSrc={imgPfp}
-                                skills={[
-                                    { text: "React.js", iconClass: "devicon-react-plain" },
-                                    { text: "Material UI", iconClass: "devicon-materialui-plain" },
-                                    { text: "Django", iconClass: "devicon-python-plain" },
-                                    { text: "MySQL", iconClass: "devicon-mysql-plain" }
-                                ]}
-                                githubUrl="https://github.com/bthk2151/PersonalFinanceProject"
-                            />
-                        </div>
+                        {projectsData.map((project, index) => (
+                            <div className="col-md-4 mb-4" key={index}>
+                                <ProjectCard
+                                    title={project.title}
+                                    description={project.description}
+                                    imgSrc={project.imgSrc}
+                                    skills={project.skills}
+                                    projectUrl={project.projectUrl}
+                                    githubUrl={project.githubUrl}
+                                />
+                            </div>
+                        ))}
                     </div>
                 </div>
             </div>
